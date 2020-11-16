1c22e78955c9036c4668614058666d251a6b373d
Alex <alex.karasek@outlook.com>
2020-11-16 13:27:37 -0500

    cleaning up dockerfile

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #start SQL Server
 sh -c "
-echo 'Sleeping 15 seconds before running setup script'
-sleep 15
+echo 'Sleeping 30 seconds before running setup script'
+sleep 30
 echo 'Starting setup script'
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P ${1} -i /tmp/restore-database.sql
 echo 'Finished setup script'
