aa0b59a7e5f4ddc4f71f9ac5322bfd645b156df9
Alex <alex.karasek@outlook.com>
2020-11-16 17:33:25 -0500

    add command

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #start SQL Server
 sh -c "
-echo 'Sleeping 45 seconds before running setup script'
-sleep 45
+echo 'Sleeping 180 seconds before running setup script'
+sleep 180
 echo 'Starting setup script'
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P ${1} -i /tmp/restore-database.sql
+exec /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P ${1} -i /tmp/restore-database.sql
 echo 'Finished setup script'
 exit" &
 exec /opt/mssql/bin/sqlservr
