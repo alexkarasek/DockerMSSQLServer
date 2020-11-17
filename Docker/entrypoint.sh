725419b0975bab527b79e327085e45d9da1e7096
Alex <alex.karasek@outlook.com>
2020-11-16 22:03:47 -0500

    updated restore scripts

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,11 +1,2 @@
-#!/bin/bash
-#start SQL Server
-sh -c "
-echo 'Sleeping 180 seconds before running setup script'
-sleep 180
-echo 'Starting setup script'
-exec /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P ${1} -i /tmp/restore-database.sql
-echo 'Finished setup script'
-exit" &
-exec /opt/mssql/bin/sqlservr
-#tail -f /dev/null
+#start SQL Server, start the script to restore the DB
+ sh /tmp/init.sh & /opt/mssql/bin/sqlservr
---
+++ Docker/init.sh
@@ -0,0 +1,14 @@
+echo "Checking SQL Server"
+STATUS=$(/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -Q "SET NOCOUNT ON; SELECT 1" -W -h-1 )
+ 
+while [ "$STATUS" != 1 ]
+do
+sleep 1s
+ 
+echo "Checking SQL Server"
+STATUS=$(/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -Q "SET NOCOUNT ON; SELECT 1" -W -h-1 )
+done
+ 
+echo "SQL UP!"
+
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -i /tmp/restore-database.sql
