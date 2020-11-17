3701d76bc1602703c8e5b051cf0352efe7f5566c
Alex <alex.karasek@outlook.com>
2020-11-17 08:38:58 -0500

    Added WWI databases

--- Docker/restore-database.sql
+++ Docker/restore-database.sql
@@ -1,5 +1,12 @@
-RESTORE DATABASE [AdventureWorks] FROM DISK = '/tmp/AdventureWorks2017.bak'
-WITH FILE = 1,
-MOVE 'AdventureWorks2017' TO '/var/opt/mssql/data/AdventureWorks.mdf',
-MOVE 'AdventureWorks2017_log' TO '/var/opt/mssql/data/AdventureWorks.ldf'
-GO
+RESTORE DATABASE WideWorldImportersDW FROM DISK = '/tmp/WideWorldImportersDW-Full.bak' 
+WITH MOVE 'WWI_Primary' TO '/var/opt/mssql/data/WideWorldImportersDW.mdf', 
+MOVE 'WWI_UserData' TO '/var/opt/mssql/data/WideWorldImportersDW_UserData.mdf', 
+MOVE 'WWI_Log' TO '/var/opt/mssql/data/WideWorldImportersDW.ldf', 
+MOVE 'WWIDW_InMemory_Data_1' TO '/var/opt/mssql/data/WideWorldImportersDW_InMemory_Data_1'
+
+
+RESTORE DATABASE WideWorldImporters FROM DISK = '/tmp/WideWorldImporters-Full.bak' 
+WITH MOVE 'WWI_Primary' TO '/var/opt/mssql/data/WideWorldImporters.mdf', 
+MOVE 'WWI_UserData' TO '/var/opt/mssql/data/WideWorldImporters_UserData.mdf', 
+MOVE 'WWI_Log' TO '/var/opt/mssql/data/WideWorldImporters.ldf', 
+MOVE 'WWI_InMemory_Data_1' TO '/var/opt/mssql/data/WideWorldImporters_InMemory_Data_1'
