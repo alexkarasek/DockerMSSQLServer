b4de3cd33abf940853d6a3021460b6bdb7118d5a
Alex <alex.karasek@outlook.com>
2020-11-17 10:20:03 -0500

    Cleanup conflicts

--- Docker/restore-database.sql
+++ Docker/restore-database.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 RESTORE DATABASE WideWorldImportersDW FROM DISK = '/tmp/WideWorldImportersDW-Full.bak' 
 WITH MOVE 'WWI_Primary' TO '/var/opt/mssql/data/WideWorldImportersDW.mdf', 
 MOVE 'WWI_UserData' TO '/var/opt/mssql/data/WideWorldImportersDW_UserData.mdf', 
@@ -11,10 +10,3 @@ WITH MOVE 'WWI_Primary' TO '/var/opt/mssql/data/WideWorldImporters.mdf',
 MOVE 'WWI_UserData' TO '/var/opt/mssql/data/WideWorldImporters_UserData.mdf', 
 MOVE 'WWI_Log' TO '/var/opt/mssql/data/WideWorldImporters.ldf', 
 MOVE 'WWI_InMemory_Data_1' TO '/var/opt/mssql/data/WideWorldImporters_InMemory_Data_1'
-=======
-RESTORE DATABASE [AdventureWorks] FROM DISK = '/tmp/AdventureWorks2017.bak'
-WITH FILE = 1,
-MOVE 'AdventureWorks2017' TO '/var/opt/mssql/data/AdventureWorks.mdf',
-MOVE 'AdventureWorks2017_log' TO '/var/opt/mssql/data/AdventureWorks.ldf'
-GO
->>>>>>> dbb13a945f57bce816612f78ab01c591408e52f6
