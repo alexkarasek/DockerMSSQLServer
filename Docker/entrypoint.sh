2d6e69a4069f1835d827755e43970a8455f31974
alexkarasek <alex@DESKTOP-5NI5QCC.localdomain>
2020-11-20 16:16:53 -0500

    config file cleanup

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,5 +1,5 @@
  #!/bin/bash
 
-#start SQL Server, start the script to restore the DB
+# start SQL Server, start the script to restore the DB
 sleep 20 & /opt/mssql/bin/sqlservr & sh /tmp/init.sh & tail -f /dev/null
 
